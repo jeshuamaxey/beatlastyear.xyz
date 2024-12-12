c77412a62c8a5b052f15ea9112d42b3bac831ea4
Jeshua Maxey <me@jeshua.co>
2024-12-12 11:12:00 +0000

    rm boilerplate

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,8 +10,8 @@ const defaultUrl = process.env.VERCEL_URL
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
+  title: "Beat Last Year",
+  description: "Show yourself and the world you're getting better every year",
 };
 
 export default function RootLayout({
