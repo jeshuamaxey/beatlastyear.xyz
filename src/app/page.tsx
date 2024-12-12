851567ae5f9009103ad5e3d347db69ad6aa9b4e5
Jeshua Maxey <me@jeshua.co>
2024-12-12 10:25:38 +0000

    get the thing building - rip out boilerplate

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,11 @@
-import Hero from "@/components/hero";
-import ConnectSupabaseSteps from "@/components/tutorial/connect-supabase-steps";
-import SignUpUserSteps from "@/components/tutorial/sign-up-user-steps";
-import { hasEnvVars } from "@/utils/supabase/check-env-vars";
 
 export default async function Index() {
   return (
     <>
-      <Hero />
-      <main className="flex-1 flex flex-col gap-6 px-4">
-        <h2 className="font-medium text-xl mb-4">Next steps</h2>
-        {hasEnvVars ? <SignUpUserSteps /> : <ConnectSupabaseSteps />}
+      <main className="flex-1 flex flex-col gap-6 px-4 text-center">
+        <h2 className="font-medium text-4xl mb-4">Beat Last Year</h2>
+        <p className="font-medium text-xl mb-2">Because you last year was a peice of shit.</p>
+        <p className="font-medium text-xl mb-2">But this year, <span className="italic">this year,</span> you're unstoppable</p>
       </main>
     </>
   );
