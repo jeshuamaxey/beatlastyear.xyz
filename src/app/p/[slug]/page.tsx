a14ccc75f5e9efe97712d0a3e28df806e1f0f120
Jeshua Maxey <me@jeshua.co>
2024-12-18 18:07:44 +0000

    tab ui for charts

--- src/app/p/[slug]/page.tsx
+++ src/app/p/[slug]/page.tsx
@@ -1,6 +1,8 @@
 import AreaChart from "@/components/area-chart";
 import BarChart from "@/components/bar-chart";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { createClient } from "@/utils/supabase/server";
+import { TabsContent } from "@radix-ui/react-tabs";
 import { redirect } from "next/navigation";
 
 export default async function ProtectedPage() {
@@ -16,8 +18,18 @@ export default async function ProtectedPage() {
 
   return (
     <div className="w-full px-4 flex flex-col gap-4">
-      <AreaChart />
-      <BarChart />
+      <Tabs defaultValue="areachart" className="w-full">
+        <TabsList className="grid w-full grid-cols-2 mb-4">
+          <TabsTrigger value="areachart">Line chart</TabsTrigger>
+          <TabsTrigger value="barchart">Bar chart</TabsTrigger>
+        </TabsList>
+        <TabsContent value="areachart">
+          <AreaChart />
+        </TabsContent>
+        <TabsContent value="barchart">
+          <BarChart />
+        </TabsContent>
+      </Tabs>
     </div>
   );
 }
